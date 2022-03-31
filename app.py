d706572f241f48210d9717775dc92a21beff03f8
18173 <18173@wc.school.nz>
2022-03-31 14:20:35 +1300

    Cleaning up

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect
 import sqlite3
 from sqlite3 import Error
 
 DB_NAME = "C:/Users/18173/OneDrive - Wellington College/13DTS/Python/Flask/smile.db"
 
 app = Flask(__name__)
+app.secret_key = "h172djaooqe7853nvbze91udha5iq7w9egnzlp123"
 
 
 def create_connection(db_file):
@@ -24,12 +25,16 @@ def render_homepage():
 
 @app.route('/menu')
 def render_menu_page():
+    # Creates the connection to the database.
     con = create_connection(DB_NAME)
 
+    # Gets the values from the product table.
     query = "SELECT id, name, description, volume, price, image FROM product"
 
+    # Executes the query to download from the database.
     cur = con.cursor()
     cur.execute(query)
+    # Converts the values into a list to be sent through the site then closes the database.
     product_list = cur.fetchall()
     con.close()
 
@@ -41,13 +46,49 @@ def render_contact_page():
     return render_template('contact.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def render_login_page():
+    if request.method == 'POST':
+        email = request.form['email'].strip().lower()
+        password = request.form['password'].strip().lower()
+
+        con = create_connection(DB_NAME)
+
+        query = "SELECT id, fname, password FROM customer WHERE email = ?"
+
+
     return render_template('login.html')
 
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def render_signup_page():
+    if request.method == 'POST':
+        # Gets all the variables the user inputted in the signup form.
+        print(request.form)
+        firstname = request.form.get('fname')
+        lastname = request.form.get('lname')
+        email = request.form.get('email')
+        password = request.form.get('pass')
+        password2 = request.form.get('pass2')
+
+        if password != password2:
+            return redirect('/signup?error=Passwords+dont+match')
+
+        if len(password) < 8:
+            return redirect('/signup?error=Passwords+must+be+8+characters+or+more')
+
+        # Creates the database connection.
+        con = create_connection(DB_NAME)
+
+        # Creates the query.
+        query = "INSERT INTO customer(id, fname, lname, email, password) VALUES(NULL,?,?,?,?)"
+
+        # Executes the query to upload to database then commits the change and closes database.
+        cur = con.cursor()
+        cur.execute(query, (firstname, lastname, email, password))
+        con.commit()
+        con.close()
+
     return render_template('signup.html')
 
 
